bbfcfc43ef46a6726746c6faf1be921ab4fa3e18
Anass200410 <as.boudhom@gmail.com>
2024-12-11 18:56:25 +0100

    Initial commit

---
+++ src/main/java/com/mybudget/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.mybudget.calculator;
+
+public class Calculator {
+    public static int add(int a ,int b){
+        return a+b;
+    }
+    public static int devision(int opG, int opD){
+        return opG/opD;
+    }
+
+}
---
+++ src/test/java/com/mybudget/calculator/CalculatorTest.java
@@ -0,0 +1,25 @@
+package com.mybudget.calculator;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator=new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+    @Test
+    void devideTest(){
+        assertEquals(2,calculator.devision(4,2));
+
+    }
+}
+
+
+
+
+
+
